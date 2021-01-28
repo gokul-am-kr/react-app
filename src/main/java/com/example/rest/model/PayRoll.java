f5bde5a2b6e13c77d3d47d432a922f0ecf12c940
GokulKrishnan <gokul.am.kr@gmail.com>
2021-01-28 13:32:24 +0530

    <Employee Api added>

---
+++ src/main/java/com/example/rest/model/PayRoll.java
@@ -0,0 +1,31 @@
+package com.example.rest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class PayRoll {
+	@Id
+private Long id;
+private int salary;
+public PayRoll(Long id, int salary) {
+	super();
+	this.id = id;
+	this.salary = salary;
+}
+public Long getId() {
+	return id;
+}
+public PayRoll() {
+	super();
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public int getSalary() {
+	return salary;
+}
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+}
---
+++ src/main/java/com/example/rest/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.rest.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
